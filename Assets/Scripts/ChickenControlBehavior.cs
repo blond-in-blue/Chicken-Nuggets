c34ca34dbf6de783e784edddfab0a0de787a5269
Eli Davis <eli.davis1995@gmail.com>
2015-10-11 11:30:03 -0600

    Addition of Hitboxes
    Simple hitbox have been added to the beak of the character and is
    enabled when dashing forward.  Try dashing at the enemy chicken and see
    what happens.  Need artificial behavior for enemy chickens now.

--- Assets/Scripts/ChickenControlBehavior.cs
+++ Assets/Scripts/ChickenControlBehavior.cs
@@ -9,6 +9,7 @@ public class ChickenControlBehavior : MonoBehaviour {
 	public float speed = 4.5f;
 	public float jumpSpeed = 8.0F;
 	public float gravity = 20.0F;
+	public float power = 5f;
 	
 	enum ChickenState {free, dashing, strafing, juicing, takingDamage}
 	ChickenState currentState = ChickenState.free;
@@ -27,42 +28,132 @@ public class ChickenControlBehavior : MonoBehaviour {
 		dashUpdate ();
 
 		movementUpdate ();
+
+		takeDamageUpdate ();
 	}
 
 	void movementUpdate(){
+
 		//simulate gravity
 		Vector3 moveDown = Vector3.zero;
 		movementDirection.y -= gravity * Time.deltaTime;
 		moveDown.y = movementDirection.y;
 		controller.Move (moveDown*Time.deltaTime);
+
 	}
 
+	void hitBoxCollisionEnter(Collision collision){
+
+
+		if (collision.gameObject.transform == this.transform) {
+			return;
+		}
+
+
+		if(currentState == ChickenState.dashing && dashingDirection == Vector3.forward){
+
+			ChickenControlBehavior otherChicken = collision.gameObject.GetComponent<ChickenControlBehavior>();
+
+			if(otherChicken != null){
+
+				otherChicken.takeDamage(power);
+
+			} else if (collision.transform.tag == "Hitbox"){
+				//play a ting noise like in smashbros.
+
+			}
+
+			stopDashing();
+		}
+
+	}
+
+
+	float damageStartTime = 0;
+
+	public void takeDamage(float damageAmount){
+		currentState = ChickenState.takingDamage;
+		damageStartTime = Time.time;
+		transform.FindChild("graphics").GetComponent<MeshRenderer>().material.color = Color.red;
+	}
+
+	void takeDamageUpdate(){
+
+		if (currentState != ChickenState.takingDamage) {
+			return;
+		}
+
+		//amount of time player is unable to move
+		float stunTime = .2f;
+
+		if (Time.time - damageStartTime > stunTime) {
+			transform.FindChild("graphics").GetComponent<MeshRenderer>().material.color = Color.white;
+			currentState = ChickenState.free;
+		}
+
+	}
+
+	/// <summary>
+	/// Has the chicken dash forward quickly and hurt things infront of it
+	/// </summary>
 	public void attackForward(){
 		beginDash (Vector3.forward);
 	}
 
+	/// <summary>
+	/// Has the chicken dash towards it's left quickly
+	/// </summary>
 	public void dashLeft(){
 		beginDash (Vector3.left);
 	}
 
+	/// <summary>
+	/// Has the chicken dash towards it's right quickly
+	/// </summary>
 	public void dashRight(){
 		beginDash (Vector3.right);
 	}
 
+	/// <summary>
+	/// Has the chicken dash backwards quickly
+	/// </summary>
 	public void dashBack(){
 		beginDash (Vector3.back);
 	}
 
+	/// <summary>
+	/// Begins the dash "animation".
+	/// Dashing forward will count as an attack.
+	/// Will dashing the player has no control.
+	/// Dashing has a small "recovery" when the player can still not move
+	/// </summary>
+	/// <param name="direction">Direction.</param>
 	void beginDash(Vector3 direction){
+
+		//if the dash is happening in no direction then we can't dash
+		if (direction.x == 0 && direction.z == 0) {
+			return;
+		}
 
 		if (!canDash ()) {
 			return;
 		}
 
+		if (direction == Vector3.forward) {
+			transform.FindChild("hitbox").GetComponent<Rigidbody>().isKinematic = false;
+			transform.FindChild("hitbox").GetComponent<SphereCollider>().enabled = true;
+		}
+
+		//Mark where we begin so we know when to end
 		dashStartTime = Time.time;
+
+		//Change state to dashing
 		currentState = ChickenState.dashing;
-		direction = 
+
+		//Set the direction of the dash for the update function
 		dashingDirection = direction;
+
+		//give alittle hop
 		movementDirection.y = 3;
 
 	}
@@ -79,24 +170,51 @@ public class ChickenControlBehavior : MonoBehaviour {
 			return;
 		}
 
-		if (Time.time - dashStartTime < .25f) {
+		//Variables for dashing and recovery
+		float dashingDuration = .25f;
+		float recoveryDuration = .15f;
+
+		//if we're still dashing
+		if (Time.time - dashStartTime < dashingDuration) {
+
+			//Move at an accelerated speed
 			move(dashingDirection, speed*3);
-		} else {
-			dashingDirection = Vector3.zero;
-			currentState = ChickenState.free;
+
+		} 
+		//if we're done dashing and have completely recovered.
+		else if(Time.time - dashStartTime > recoveryDuration+dashingDuration){
+
+			//exit the dashing state
+			stopDashing();
+
 		}
 
 	}
-	
+
+	void stopDashing(){
+		dashingDirection = Vector3.zero;
+		currentState = ChickenState.free;
+		transform.FindChild("hitbox").GetComponent<Rigidbody>().isKinematic = true;
+		transform.FindChild("hitbox").GetComponent<SphereCollider>().enabled = false;
+	}
+
+	/// <summary>
+	/// Makes sure the player is always looking at the target if there is one.
+	/// </summary>
 	void lookAtTargetUpdate () {
 
+		//If we don't have a target we can't look at it
 		if (target == null) {
 			return;
 		}
 
-		transform.LookAt(target.transform);
+		//A smooth rotation
+		transform.rotation  = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(target.transform.position - transform.position), Time.deltaTime*10);
 	}
 
+	/// <summary>
+	/// Moves the chicken counter clockwise around it's target if it has one
+	/// </summary>
 	public void strafeRight ()
 	{
 		if (!canMove ()) {
@@ -105,6 +223,9 @@ public class ChickenControlBehavior : MonoBehaviour {
 		move(Vector3.right, speed);
 	}
 
+	/// <summary>
+	/// Moves the chicken clockwise around it's target if it has one
+	/// </summary>
 	public void strafeLeft ()
 	{
 		if (!canMove ()) {
@@ -113,6 +234,9 @@ public class ChickenControlBehavior : MonoBehaviour {
 		move(Vector3.left, speed);
 	}
 
+	/// <summary>
+	/// Moves the chicken backwards.
+	/// </summary>
 	public void moveBackward()
 	{
 		if (!canMove ()) {
@@ -121,6 +245,9 @@ public class ChickenControlBehavior : MonoBehaviour {
 		move(Vector3.back, speed);
 	}
 
+	/// <summary>
+	/// Moves the chicken forward.
+	/// </summary>
 	public void moveForward()
 	{
 		if (!canMove ()) {
@@ -169,6 +296,11 @@ public class ChickenControlBehavior : MonoBehaviour {
 
 	}
 
+
+	/// <summary>
+	/// Determines whether or not the chicken is allowed to dash in it's current state of affairs..
+	/// </summary>
+	/// <returns><c>true</c>, if chicken can dash, <c>false</c> otherwise.</returns>
 	bool canDash(){
 		if (currentState == ChickenState.free && controller.isGrounded) {
 			return true;
---
+++ Assets/Scripts/HitboxBehavior.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitboxBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision collision){
+		SendMessageUpwards ("hitBoxCollisionEnter",collision);
+	}
+}
