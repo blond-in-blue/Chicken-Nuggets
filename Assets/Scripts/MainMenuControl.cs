1281872b5a0cd71093ba445f58ed0b28410f4b3d
Blond-in-Blue <hholder@me.com>
2015-10-11 22:15:19 -0500

    Start making the main menu
    Nothing works right now, but here’s the framework.

---
+++ Assets/Scripts/MainMenuControl.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MainMenuControl : MonoBehaviour {
+
+	public Menu CreditsMenu;
+	public Button CreditsButton;
+	public Button TrainingButton;
+	public Button CockFightButton;
+	public Button ChickenWafflesButton;
+	public Button ChickenSkewerButton;
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+		CreditsButton = CreditsButton.GetComponent<Button> ();
+		TrainingButton = TrainingButton.GetComponent<Button> ();
+		CockFightButton = CockFightButton.GetComponent<Button> ();
+		ChickenWafflesButton = ChickenWafflesButton.GetComponent<Button> ();
+		ChickenSkewerButton = ChickenSkewerButton.GetComponent<Button> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OpenCredits() {
+		Application.OpenCreditsMenu();
+	}
+	
+	void OpenTraining() {
+		Application.LoadLevel(Training1);
+	}
+	
+	void OpenCockFight() {
+		Application.LoadLevel(CockFight1);
+	}
+	
+	void OpenChickenWaffles() {
+		
+	}
+	
+	void OpenChickenSkewer() {
+		
+	}
+}
